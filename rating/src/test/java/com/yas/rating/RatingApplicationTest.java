59619a2259381695915625f07433cd900021532a
emanhthangngot <emanhthangngot@gmail.com>
2026-05-02 01:16:52 +0700

    test: Convert integration tests to unit tests for rating service and improve coverage to 76%

--- rating/src/test/java/com/yas/rating/RatingApplicationTest.java
+++ rating/src/test/java/com/yas/rating/RatingApplicationTest.java
@@ -1,9 +1,7 @@
 package com.yas.rating;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class RatingApplicationTest {
 
     @Test
